34e678e29c96b52523a5b3e4e2a77135850d8766
MeesamBukhari <meesamturab@gmail.com>
2024-07-23 18:20:41 +0500

    weather app in ios design

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./public/**/*.{html,js}", "./src/**/*.{css,js}"];
+export const theme = {
+  extend: {
+    colors: {
+      midnightExpress: "#14213D",
+      darkTangerine: "#FCA311",
+      pumpkin: "#FF7F11",
+      desaturatedPink: "#542344",
+    },
+  },
+};
+export const plugins = [];
